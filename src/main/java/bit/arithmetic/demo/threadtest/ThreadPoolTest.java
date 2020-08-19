a7100d06ea37b2f306f7a8c897def163527e381c
rockRust <rust18633256516@163.com>
2020-08-19 20:38:22 +0800

    Add threadpool

---
+++ src/main/java/bit/arithmetic/demo/threadtest/ThreadPoolTest.java
@@ -0,0 +1,44 @@
+package bit.arithmetic.demo.threadtest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author jiaosong
+ * @desc
+ * @date 2020/4/25
+ */
+public class ThreadPoolTest {
+    ThreadPoolExecutor threadPool = new ThreadPoolExecutor(2, 5,
+            1000, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+
+    AtomicInteger count = new AtomicInteger(0);
+
+    private void add() throws ExecutionException, InterruptedException {
+        List<Future> futures = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Future<?> future = threadPool.submit(() -> {
+                count.incrementAndGet();
+                System.out.println("add...");
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+
+                }
+            });
+            futures.add(future);
+        }
+        for (Future future:futures){
+            future.get();
+        }
+
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ThreadPoolTest test = new ThreadPoolTest();
+        test.add();
+        System.out.println("result:" + test.count.get());
+    }
+}
