9c123d735fbc513ce2b8783aea76d4a4d2dd8aeb
rockRust <rust18633256516@163.com>
2020-12-22 16:58:38 +0800

    Add phone msg

---
+++ src/main/java/bit/arithmetic/demo/message/GdMessageClient.java
@@ -0,0 +1,28 @@
+package bit.arithmetic.demo.message;
+
+/**
+ * @author jiaosong
+ * @desc
+ * @date 2020/12/22
+ */
+public class GdMessageClient implements MessageClient {
+    @Override
+    public void connect() {
+
+    }
+
+    @Override
+    public void disConnect() {
+
+    }
+
+    @Override
+    public void reconnect() {
+
+    }
+
+    @Override
+    public void sendMsg() {
+
+    }
+}
