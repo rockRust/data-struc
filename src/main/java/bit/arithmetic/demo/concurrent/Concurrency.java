8910dc05c1ce10d58d5438ad3c57b5916f68168f
rockRust <rust18633256516@163.com>
2021-04-06 09:57:53 +0800

    Add  list remove

--- src/main/java/bit/arithmetic/demo/concurrent/Concurrency.java
+++ src/main/java/bit/arithmetic/demo/concurrent/Concurrency.java
@@ -1,9 +1,7 @@
 package bit.arithmetic.demo.concurrent;
 
-import java.util.Collections;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * @author jiaosong
@@ -12,12 +10,25 @@ import java.util.concurrent.CopyOnWriteArrayList;
  */
 public class Concurrency {
     public static void main(String[] args) {
-        ConcurrentHashMap<Object, Object> location = new ConcurrentHashMap<>();
-        location.put("1", 1);
-        location.put("2", 2);
-        Map<Object, Object> objectObjectMap = Collections.unmodifiableMap(location);
-        System.out.println("result:" + objectObjectMap.get("1"));
-        location.put("1", 3);
-        System.out.println("result:" + objectObjectMap.get("1"));
+//        ConcurrentHashMap<Object, Object> location = new ConcurrentHashMap<>();
+//        location.put("1", 1);
+//        location.put("2", 2);
+//        Map<Object, Object> objectObjectMap = Collections.unmodifiableMap(location);
+//        System.out.println("result:" + objectObjectMap.get("1"));
+//        location.put("1", 3);
+//        System.out.println("result:" + objectObjectMap.get("1"));
+
+        String s = "222;333;333;444";
+        String[] split = s.split(";");
+        String remove = "333";
+        List<String> list = Arrays.asList(split);
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            String next = iterator.next();
+            if (next.equals(remove)) {
+                iterator.remove();
+            }
+        }
+        System.out.println("" + list);
     }
 }
